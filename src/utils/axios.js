58b2fa78d9087c07328c45579c494ad3c19513b3
AhmidouA <ahmedakkioui75@gmail.com>
2023-09-08 13:37:09 +0200

    Init Aromatika Front Project

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://ahmidoua-server.eddi.cloud:8080';
+const BASE_URL = 'https://aromatika-back-api.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
