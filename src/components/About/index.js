35c3219bc6a723f6e3dc697b8f7f718028d01859
AhmidouA <ahmedakkioui75@gmail.com>
2023-09-13 14:36:26 +0200

    Button, link and title category bug fixed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,7 +17,7 @@ function About() {
                     <div className="About-profile-left-description-container">
                         <h2 className="About-profile-left-description-name">Anis</h2>
                         <h3 className="About-profile-left-description-role">Product owner et concepteur du projet</h3>
-                        <p className="About-profile-left-description-resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="About-profile-left-description-resume">En cours de rédaction...</p>
                     </div>
                 </div>
                 <div className="About-profile-right">
@@ -25,7 +25,7 @@ function About() {
                     <div className="About-profile-right-description-container">
                         <h2 className="About-profile-right-description-name">Sophie</h2>
                         <h3 className="About-profile-right-description-role">Scrum master</h3>
-                        <p className="About-profile-right-description-resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="About-profile-right-description-resume">En cours de rédaction...</p>
                     </div>
                 </div>
                 <div className="About-profile-left">
@@ -33,7 +33,7 @@ function About() {
                     <div className="About-profile-left-description-container">
                         <h2 className="About-profile-left-description-name">Ahmed</h2>
                         <h3 className="About-profile-left-description-role">Lead dev back</h3>
-                        <p className="About-profile-left-description-resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="About-profile-left-description-resume">Passioné par la culture asiatique, les voyages et le code. Après plusieurs années en tant que chef de projet en E-commece, j'ai décidé de me reconvertir dans le développement web.</p>
                     </div>
                 </div>
                 <div className="About-profile-right">
