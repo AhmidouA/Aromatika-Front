c7d2ca556186a0b2ef35dee1e51edbbfdc6836d2
AhmidouA <ahmedakkioui75@gmail.com>
2023-09-19 15:09:12 +0200

    Feature : update username pseudo in Parameters/Profile Done

--- src/components/Parameters/Profile/index.js
+++ src/components/Parameters/Profile/index.js
@@ -70,8 +70,10 @@ function ParametersProfile({ userId, userName, userImage }) {
     /**       Les states */
     // state profile
     const [profile, setProfile] = useState(null)
-    // state new username
+    // state pseudo depuis le profile
     const [pseudo, setPseudo] = useState("");
+    // state pseudoInput 
+    const [pseudoInputValue, setPseudoInputValue] = useState(""); 
     
 
     /**       Les methode */
@@ -130,18 +132,21 @@ function ParametersProfile({ userId, userName, userImage }) {
                 });
 
                 console.log("useEffect Dans le params profile", response)
-                // Mettre à jour le pseudonyme dans l'état local à partir du localStorage
-                const storedUsername = localStorage.getItem("username");
-                // Vérifie si un pseudonyme est stocké dans le localStorage
-                if (storedUsername) {
-                    // Met à jour l'état profile en utilisant la fonction setProfile
-                    // en copiant d'abord les valeurs précédentes (prevProfile) avec ...
-                    // puis en remplaçant userName par la valeur stockée localement (storedUsername)
-                    setProfile((prevProfile) => ({
-                        ...prevProfile, // Copie les valeurs précédentes de prevProfile
-                        userName: storedUsername // Remplace userName par storedUsername
 
-                    }))
+                // Mettre à jour le pseudonyme dans l'état local à partir du localStorage
+                // On commence par récupérer la valeur stockée dans le localStorage avec la clé 'username'
+                const storedUsername = localStorage.getItem('username');
+
+                // On vérifie si storedUsername a une valeur (c'est-à-dire qu'elle n'est pas null ou undefined)
+                if (storedUsername) {
+                    // Si storedUsername a une valeur, cela signifie qu'il y a un pseudonyme stocké dans le localStorage.
+                    // Nous mettons alors à jour l'état local (state) 'pseudo' avec cette valeur.
+                    setPseudo(storedUsername);
+                } else {
+                    // Si storedUsername est null ou undefined, cela signifie qu'il n'y a pas de pseudonyme stocké dans le localStorage.
+                    // Dans ce cas, nous utilisons le pseudonyme provenant de la réponse du serveur (response.data.userName).
+                    // Nous mettons à jour l'état local (state) 'pseudo' avec cette valeur.
+                    setPseudo(response.data.userName);
                 }
                 
 
@@ -190,7 +195,7 @@ function ParametersProfile({ userId, userName, userImage }) {
 
             <div className="Parameters-profile-pseudo">
                 <form className="Parameters-profile-pseudo-form" onSubmit={handleSubmitUsername}>
-                    <label className="Parameters-profile-pseudo-form-label" for="pseudo">Mon pseudo actuel : {profile?.userName}</label>
+                    <label className="Parameters-profile-pseudo-form-label" for="pseudo">Mon pseudo actuel : {pseudo}</label>
                     <div className="Parameters-profile-pseudo-form-container">
                         <input
                             className="Parameters-profile-pseudo-form-input"
@@ -198,8 +203,8 @@ function ParametersProfile({ userId, userName, userImage }) {
                             id="pseudo"
                             name="pseudo"
                             placeholder="Entrer un nouveau pseudo"
-                            value={pseudo}
-                            onChange={(event) => setPseudo(event.target.value)}
+                            value={pseudoInputValue}
+                            onChange={(event) => setPseudoInputValue(event.target.value)}
                         />
                         <input className="Parameters-profile-pseudo-form-submit" type="submit" value="Valider" />
                     </div>
